86bddf1ef4370c76f0d9aa671d9a5d87c397f52f
Rian Ribeiro <vadergames428@gmail.com>
2024-08-06 19:12:35 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Rian Ribeiro com blusa preta e iluminaçao laranja"
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Rian Ribeiro com blusa preta e iluminação verde"
+    )
+  }
+}
